e634bb35e96643d527327c50822c716089729fa2
wangziwei <35870166+maoyiyiyi@users.noreply.github.com>
2021-06-12 22:05:14 -0400

    modify hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br></br>My Personal Porfolio
+      </SectionTitle>
+      <SectionText>hire me plz</SectionText>
+      <Button>Learn more</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
